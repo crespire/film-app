614dd58cebe97280b4ff7e1be5d55c707917f9e2
crespire <hello@crespire.dev>
2024-03-03 15:55:42 -0500

    Add form for film roll and remove unused views

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,13 +1,17 @@
 class RollsController < ApplicationController
-  before_action :roll, except: [:index]
+  before_action :roll, except: %i[index new create]
 
   def index; end
 
-  def new; end
+  def new
+    @roll = Roll.new
+  end
 
   def edit; end
 
-  def create; end
+  def create
+    @roll = Roll.new(roll_params)
+  end
 
   def update; end
 
@@ -19,5 +23,9 @@ class RollsController < ApplicationController
 
   def roll
     @roll ||= Roll.find(params[:id] || params[:roll_id])
+  end
+
+  def roll_params
+    params.require(:roll).permit(:camera, :film_name, :film_speed, :status, :film_type, :notes)
   end
 end
